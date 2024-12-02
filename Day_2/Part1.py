618f55f90ef71b701a962856bb0b9332449b92f0
wtvar <wtvariwnt@gmail.com>
2024-12-02 15:29:44 +0000

    Create Part1.py

---
+++ Day_2/Part1.py
@@ -0,0 +1,60 @@
+file_name = "input.txt"
+
+left_list = []
+right_list = []
+
+        
+        
+def check_higher(list):
+	current = 0
+	prior = 0
+	higher = None
+	for val in list:
+		current = int(val)
+		if prior == 0:
+			prior = current
+			pass
+			
+		elif current > prior and ((current -  prior) <= 3):
+			prior = current
+			higher = True
+		else: 
+		    higher = False
+		    break
+	return higher
+	
+	
+def check_lower(list):
+	current = 0
+	prior = float('inf')
+	higher = None
+	for val in list:
+		current = int(val)
+		if prior == float:
+			prior = current
+			pass
+		elif current < prior and ((prior - current) <= 3):
+			prior = current
+			higher = True
+		else: 
+		    higher = False
+		    break
+	return higher
+			
+		
+with open(file_name) as text:
+    safe = 0
+    for line in text:
+        data = line.strip().split('\n')
+        print(data)
+        current = 0
+        prior = 0
+        line_list = data[0].split(" ")
+        if check_higher(line_list):
+        	safe += 1
+        elif check_lower(line_list):
+        	safe += 1
+        print(f'safe: {safe}')
+        	
+    print(f'final safe: {safe}')
+		
